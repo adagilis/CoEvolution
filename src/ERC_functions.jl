24021deddffef8757f2eb21e4b2a0801bda57dfb
adagilis <adagilis@gmail.com>
2024-12-19 11:42:47 -0500

    Added reorder_branches

--- src/ERC_functions.jl
+++ src/ERC_functions.jl
@@ -10,30 +10,27 @@ using DataFrames
 using Statistics
 using HypothesisTests
 using CSV
-using RCall
 
-function calculate_ERC(gene_id_1::Int64,gene_id_2::Int64,species_tree,cutoff=-5)
-    scale_rates=deepcopy(species_tree)
+function calculate_ERC(gene_id_1::Int64,gene_id_2::Int64,species_tree::RootedTree,cutoff=-5)
+    template = deepcopy(species_tree)
     gene1 = open(parsenewick,trees[gene_id_1])
     gene2 = open(parsenewick,trees[gene_id_2])
     tips_both=intersect(getleafnames(gene1),getleafnames(gene2))
-    keeptips!(gene1,tips_both)
-    keeptips!(gene2,tips_both)
-    keeptips!(scale_rates,tips_both)
-    l1 = [e.length for e in getbranches(gene1)]
-    l2 = [e.length for e in getbranches(gene2)]
-    lt = [e.length for e in getbranches(scale_rates)]
-    l1 = l1 ./ lt
-    l2 = l2 ./ lt
-    kept_edges = intersect(findall(<(cutoff),l1),findall(<(cutoff),l2))
+    template = reorder_tree(template,tips_both)
+    gene1 = reorder_tree(gene1,tips_both)
+    gene2 = reorder_tree(gene2,tips_both)
+    scale_rates = get_branch_lengths(template)
+    branches_1 = get_branch_lengths(gene1) ./ scale_rates
+    branches_2 = get_branch_lengths(gene2) ./ scale_rates
+    kept_edges = intersect(findall(<(cutoff),branches_1),findall(<(cutoff),branches_2))
     if(length(kept_edges)>3)
-        l1 = (l1 .- mean(l1)) ./ (std(l1))
-        l2 = (l2 .- mean(l2)) ./ (std(l2))
-        r2=cor(l1[kept_edges],l2[kept_edges])
-        pval=pvalue(CorrelationTest(l1[kept_edges],l2[kept_edges]))
+        z_1 = zscore(branches_1)
+        z_2 = zscore(branches_2)
+        r2 = cor(z_1[kept_edges],z_2[kept_edges])
+        pval = pvalue(CorrelationTest(z_1[kept_edges],z_2[kept_edges]))
     else 
-        r2=0
-        pval=1
+        r2 = 0
+        pval = 1
     end
     return(Dict(:i => trees[gene_id_1],:j => trees[gene_id_2],:n_edges=>length(kept_edges),:r2 => r2,:pval => pval))
 end
@@ -48,4 +45,17 @@ function runERC()
             end
         end
     return(ERC_res)
+end
+
+function zscore(x)
+    return((x .- mean(x))./ std(x))
+end
+
+function get_branch_lengths(tree)
+    return([e.length for e in getbranches(tree)])
+end
+
+function reorder_tree(tree::RootedTree,tips)
+    sort!(keeptips!(tree,tips))
+    return(parsenewick(Phylo.outputtree(tree,Newick())))
 end
