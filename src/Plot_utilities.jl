b714e39c1075db009ca257d0a4475b6bd2c0d927
adagilis <adagilis@uconn.edu>
2025-08-20 13:48:45 -0400

    graph_clustering and plotting

--- src/Plot_utilities.jl
+++ src/Plot_utilities.jl
@@ -7,3 +7,28 @@ function chr_order_pos(position,chr,chr_order,chr_lengths)
     end
     return(position)
 end
+
+
+function graph_plot(graph,dataframe)
+    
+    #plot nodes
+    cols=scale_colors(dataframe.community)
+    sizes=scale_sizes(dataframe.degree)
+    network_plot= @df dataframe scatter(:x,:y,
+    c=cols,
+    legend=false,
+    size=sizes;
+    axis=([],false),
+    markerstrokewidth=0)
+
+    #plot edges
+    id_from = missing
+    id_to = missing
+    df.from_x = dataframe.x[id_from]
+    df.from_y = dataframe.y[id_from]
+    df.to_x = dataframe.x[id_to]
+    df.to_y = dataframe.y[id_to]
+    plot!(
+        [([i.from_x,i.to_x],[i.from_y,i.to_y])  for i in eachrow(df)],
+        c=:black,alpha=0.5,legend=false)
+end
